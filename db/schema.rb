26127f80f624586368de162332631c27bcc2ee26
Philip Lee-Chin <phil.leechin@outlook.com>
2020-04-30 21:07:17 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_04_30_192103) do
 
-  create_table "movies", force: :cascade do |t|
+  create_table "movie", force: :cascade do |t|
     t.string "title"
     t.integer "release_date"
     t.string "director"
